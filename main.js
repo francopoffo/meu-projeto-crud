fa7eb427cc9b199af23870f6f34a8df31ce9c1b3
francopoffo <franco_sutter@yahoo.com.br>
2022-11-23 17:34:10 -0300

    Adicionando atualização no localStorage na função de atualizar

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const $meuFormulario = document.querySelector('#form');
-const $listaTarefas = document.querySelector('#listaTarefas')
+const $meuFormulario = document.querySelector("#form");
+const $listaTarefas = document.querySelector("#listaTarefas");
 let tarefasTotais = JSON.parse(localStorage.getItem("Tarefas")) || [];
 
-
 const modelo = {
-        tarefas: [
-        {
-            id: 1,
-            owner: 'francopoffo',
-            content: 'CTD'
-        }
-    ],
-    criaTarefa(tarefa){
-                
-        $listaTarefas.insertAdjacentHTML('afterbegin', `
+  tarefas: [
+    {
+      id: 1,
+      owner: "francopoffo",
+      content: "CTD",
+    },
+  ],
+  criaTarefa(tarefa) {
+    $listaTarefas.insertAdjacentHTML(
+      "afterbegin",
+      `
             <li data-id="${tarefa.id}">
             <button class="botao__apagar">X</button>
             <span contenteditable>
@@ -21,68 +21,62 @@ const modelo = {
             </span>
             </li>
     
-        `)
-        
-    },
-    leTarefa(){
-        modelo.tarefas.forEach(({id, owner, content}) => {
-            modelo.criaTarefa({id: id, owner: owner, content: content});
-            
-        })
-        for(const tarefa of tarefasTotais){
-            modelo.criaTarefa(tarefa)
-        }
-
-       
-    },
-    apagaTarefa(id) {
-        const listaDeTarefasAtualizada = modelo.tarefas.filter((tarefaAtual) => {
-            return tarefaAtual.id !== Number(id);
-        })
-        modelo.tarefas = listaDeTarefasAtualizada;
-
-        const listaDeTarefasLocalStorage = tarefasTotais.filter((tarefaAtual) => {
-            return tarefaAtual.id !== Number(id);
-        })
-        tarefasTotais = listaDeTarefasLocalStorage;
-        
-        localStorage.setItem("Tarefas", JSON.stringify(tarefasTotais));
-
-    },
-    atualizaTarefa(id, novoConteudo) {
-        const tarefaAtualizada = modelo.tarefas.find((post) => {
-            return post.id === Number(id);
-        });
-        tarefaAtualizada.content = novoConteudo
+        `
+    );
+  },
+  leTarefa() {
+    modelo.tarefas.forEach(({ id, owner, content }) => {
+      modelo.criaTarefa({ id: id, owner: owner, content: content });
+    });
+    for (const tarefa of tarefasTotais) {
+      modelo.criaTarefa(tarefa);
     }
-    
-    
-}
+  },
+  apagaTarefa(id) {
+    const listaDeTarefasAtualizada = modelo.tarefas.filter((tarefaAtual) => {
+      return tarefaAtual.id !== Number(id);
+    });
+    modelo.tarefas = listaDeTarefasAtualizada;
+
+    const listaDeTarefasLocalStorage = tarefasTotais.filter((tarefaAtual) => {
+      return tarefaAtual.id !== Number(id);
+    });
+    tarefasTotais = listaDeTarefasLocalStorage;
+
+    localStorage.setItem("Tarefas", JSON.stringify(tarefasTotais));
+  },
+  atualizaTarefa(id, novoConteudo) {
+    const tarefaAtualizada = tarefasTotais.find((tarefa) => {
+      return tarefa.id === Number(id);
+    });
+
+    tarefaAtualizada.content = novoConteudo;
+    localStorage.setItem("Tarefas", JSON.stringify(tarefasTotais));
+  },
+};
 
 // CREATE
 
+$meuFormulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let $conteudoDaTarefa = document.querySelector("#inConteudo");
 
-$meuFormulario.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    let $conteudoDaTarefa = document.querySelector('#inConteudo');
+  idDoPost = Date.now();
 
-    idDoPost = Date.now();
+  const novaTarefa = {
+    id: idDoPost,
+    owner: "francopoffo",
+    content: $conteudoDaTarefa.value,
+  };
 
-    const novaTarefa = {
-        id: idDoPost,
-        owner: 'francopoffo',
-        content: $conteudoDaTarefa.value
-    }
-    
-    modelo.criaTarefa(novaTarefa);
-    tarefasTotais.push(novaTarefa);
+  modelo.criaTarefa(novaTarefa);
+  tarefasTotais.push(novaTarefa);
 
-    localStorage.setItem("Tarefas", JSON.stringify(tarefasTotais));
+  localStorage.setItem("Tarefas", JSON.stringify(tarefasTotais));
 
-    $meuFormulario.reset();
-    $meuFormulario.inConteudo.focus();
-})
-
+  $meuFormulario.reset();
+  $meuFormulario.inConteudo.focus();
+});
 
 //READ
 
@@ -90,29 +84,25 @@ modelo.leTarefa();
 
 //UPDATE
 
-$listaTarefas.addEventListener('input', function (dados) {
-    
-    const itemAtual = dados.target;
-    const id = itemAtual.parentNode.getAttribute('data-id');
+$listaTarefas.addEventListener("input", function (dados) {
+  const itemAtual = dados.target;
+  const id = itemAtual.parentNode.getAttribute("data-id");
 
-    modelo.atualizaTarefa(id, itemAtual.innerText)
+  modelo.atualizaTarefa(id, itemAtual.innerText);
 });
-
 
 //DELETE
 
-$listaTarefas.addEventListener('click', function(dados) {
-    const itemAtual = dados.target;
-    const botaoApaga = dados.target.classList.contains('botao__apagar');
+$listaTarefas.addEventListener("click", function (dados) {
+  const itemAtual = dados.target;
+  const botaoApaga = dados.target.classList.contains("botao__apagar");
 
-    if (botaoApaga) {
-        
-        const id = itemAtual.parentNode.getAttribute('data-id');
+  if (botaoApaga) {
+    const id = itemAtual.parentNode.getAttribute("data-id");
 
-
-        // Manipula o lado do arquivo
-        modelo.apagaTarefa(id);
-        // Manipula o HTML
-        itemAtual.parentNode.remove();
-    }   
-})
+    // Manipula o lado do arquivo
+    modelo.apagaTarefa(id);
+    // Manipula o HTML
+    itemAtual.parentNode.remove();
+  }
+});
